d265be534951f72d71c35076a70a64f39874319c
Clementine <clementineyang1@gmail.com>
2025-07-11 13:19:02 -0400

    Implement setCurrentUser in InMemory DAO and stub in File DAO

--- src/main/java/use_case/login/LoginInteractor.java
+++ src/main/java/use_case/login/LoginInteractor.java
@@ -19,17 +19,18 @@ public class LoginInteractor implements LoginInputBoundary {
     public void execute(LoginInputData loginInputData) {
         final String username = loginInputData.getUsername();
         final String password = loginInputData.getPassword();
+
         if (!userDataAccessObject.existsByName(username)) {
             loginPresenter.prepareFailView(username + ": Account does not exist.");
-        }
-        else {
+        } else {
             final String pwd = userDataAccessObject.get(username).getPassword();
             if (!password.equals(pwd)) {
                 loginPresenter.prepareFailView("Incorrect password for \"" + username + "\".");
-            }
-            else {
-
+            } else {
                 final User user = userDataAccessObject.get(loginInputData.getUsername());
+
+                // Task 2.1: Set current user for session tracking
+                userDataAccessObject.setCurrentUser(user.getName());
 
                 final LoginOutputData loginOutputData = new LoginOutputData(user.getName(), false);
                 loginPresenter.prepareSuccessView(loginOutputData);
